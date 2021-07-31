de1edb64542d154ba51ef8a1b8af9515ad832845
ShauryaKharb <73641410+ShauryaKharb@users.noreply.github.com>
2021-07-31 19:16:25 +0530

    first commit

--- screens/insta.js
+++ screens/insta.js
@@ -1,20 +1,13 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
-import { LinearTextGradient } from 'react-native-text-gradient'
+// import { LinearTextGradient } from 'react-native-text-gradient'
 
 export default class Insta extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <LinearGradient colors={['#fff', '#ff0']}>
-          <LinearTextGradient
-            style={({ fontWeight: 'bold' }, { fontSize: 30 })}
-            colors={['#000', '#ff0']}
-          >
-            <Text style={[styles.text]}>Instagram</Text>
-          </LinearTextGradient>
-        </LinearGradient>
+        <Text style={[styles.text]}>Instagram</Text>
       </View>
     )
   }
